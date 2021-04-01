9d15541b165eb0f1acf61ccc9786e4c1b74a387b
sini5971 <sini5971@naver.com>
2021-04-01 20:57:08 +0900

    week05

---
+++ week05/hamburger/js/main.js
@@ -0,0 +1,8 @@
+//햄버거 바 아이콘 선택자
+const menuOpenEl =document.getElementById('btn--open')
+//닫는 아이콘 선택자
+const menuOpenEl =document.getElementById('btn--close')
+
+menuOpenEl.addEventListener("click" , function() {
+    menuOpenEl.style.display="none"
+})
